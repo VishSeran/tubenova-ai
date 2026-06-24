a7ea43fc74466096a6a51fb41048cd1445f9417d
VishSeran <vishwa.sovis@gmail.com>
2026-06-24 10:46:01 +0530

    main-summary generate updated

--- modules/main.py
+++ modules/main.py
@@ -2,9 +2,50 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_huggingface import ChatHuggingFace
 from modules.logger import get_logger
+from langchain_classic.globals import set_verbose
+
+set_verbose(True)
 
 logger = get_logger("main-logger")
 
+def summary_generate(process_transcript, llm:ChatHuggingFace):
+    
+    try:
+        if not process_transcript:
+            raise ValueError("transcript is empty")
+        
+        template = """
+            <|begin_of_text|><|start_header_id|>system<|end_header_id|>
+            You are an AI assistant tasked with summarizing YouTube video transcripts
+            Instructions:
+            
+            1. Summarize the transcript in a single concise paragraph.
+            2. Ignore any timestamps in your summary.
+            3. Focus on the spoken content (Text) of the video.
+            
+            Note: In the transcript, "Text" refers to the spoken words in the video,
+            Please summarize the following YouTube video transcript:
+            
+            {transcript}<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+        """
+        
+        prompt_template = PromptTemplate(
+            template=template,
+            input_variables=['transcript']
+        )
+        
+        summary_chain = prompt_template | llm | StrOutputParser()
+        
+        
+        
+    except ValueError as e:
+        logger.error(f"Value error: {e}")
+        return None
+    
+    except Exception as e:
+        logger.error(f"Error in retriever: {e}")
+        return None     
+    
 
 def chat_with_llm(query, content, llm:ChatHuggingFace):
     
