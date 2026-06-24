6834aef204c768fd13bdb8632b664bb70cbb04a1
VishSeran <vishwa.sovis@gmail.com>
2026-06-24 10:26:58 +0530

    model-config-chat with llm updated

--- modules/model_config.py
+++ modules/model_config.py
@@ -137,32 +137,48 @@ def retrieve(query, vectorstore, k=4):
 
 def chat_with_llm(query, content, llm:ChatHuggingFace):
     
-    template = """
-    You are an expert assistant.
-
-    Video Content:
-    {content}
-
-    Answer the following question using only the information provided in the video content.
-
-    Question:
-    {question}
-
-    If the answer cannot be found in the video content, respond with:
-    "I don't know based on the provided video content."
-    """
-    
-    prompt_template = PromptTemplate(
-        template=template,
-        input_variables=['content', 'question']
+    try:
         
-    )
+        if not query:
+            raise ValueError("query is empty")
+        
+        if not content:
+            raise ValueError("Video content is empty")
+        
+        template = """
+        You are an expert assistant.
+
+        Video Content:
+        {content}
+
+        Answer the following question using only the information provided in the video content.
+
+        Question:
+        {question}
+
+        If the answer cannot be found in the video content, respond with:
+        "I don't know based on the provided video content."
+        """
+        
+        prompt_template = PromptTemplate(
+            template=template,
+            input_variables=['content', 'question']
+            
+        )
+        
+        chat_chain = prompt_template | llm | StrOutputParser() 
+        
+        answer = chat_chain.invoke(input={"content": content, "question": query})
+        
+        return answer
     
-    chat_chain = prompt_template | llm | StrOutputParser()
+    except ValueError as e:
+        logger.error(f"Value error: {e}")
+        return None
     
-    answer = chat_chain.invoke(input={"content": content, "question": query})
-    
-    return answer
-    
+    except Exception as e:
+        logger.error(f"Error in retriever: {e}")
+        return None
+        
     
     
